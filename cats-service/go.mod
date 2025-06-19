d075927d5dece34b0cf2c8105f7b75765b802c9a
Sai7xp <saisumanth2303@gmail.com>
2025-06-19 12:39:29 +0530

    import utils from github and local

--- cats-service/go.mod
+++ cats-service/go.mod
@@ -1,3 +1,7 @@
-module cats-service
+module github.com/sai7xp/go-microservice-template/cats-service
 
 go 1.24.0
+
+// replace github.com/sai7xp/go-microservice-template/common-utils => ../common-utils
+
+require github.com/sai7xp/go-microservice-template/common-utils v0.0.0-20250619070154-4225c64125ef
--- cats-service/main.go
+++ cats-service/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	commonutils "github.com/sai7xp/go-microservice-template/common-utils/crypto/aes"
+)
+
+func main() {
+	key, _ := commonutils.GenerateSymmetricKey()
+	encryptedString := commonutils.EncryptData(key, []byte("hello"))
+	fmt.Println(encryptedString)
+	decryptedData := commonutils.DecryptData("swOwWa9MfseTK7q2FkbRjseWCntJGoUypcI+8w91Ico=", "5b/QzGVZY/4xtsVwTU9HQu+XVhzA6jXm+QDAUvOLpMSC")
+	fmt.Println(string(decryptedData))
+}
